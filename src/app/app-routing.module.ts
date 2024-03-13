d8314efa977398cd6a1813e8341f29e564f2d992
Denisa Kita <denisakita7@gmail.com>
2024-03-13 16:58:43 +0100

    add new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,22 +7,22 @@ const routes: Routes = [
   {
     path: 'profile/:id',
     loadComponent: () => import('./pages/profile/profile.component').then(m => m.ProfileComponent),
-    title: (route:ActivatedRouteSnapshot) =>`Finder | Profile for ${route.paramMap.get('id')}`,
+    title: (route: ActivatedRouteSnapshot) => `Finder | Profile for ${route.paramMap.get('id')}`,
   },
   {
     path: 'players',
-    loadComponent: () => import('./pages/players/players.component').then(m => m.PlayersModule),
+    loadComponent: () => import('./pages/players/players.module').then(m => m.PlayersModule),
     title: 'Finder | All Players',
     data: {preload: true}
   },
   {
     path: 'leaderboards',
-    loadComponent: () => import('./pages/leaderboards/leaderboards.component').then(m => m.LeaderboardsModule),
+    loadComponent: () => import('./pages/leaderboards/leaderboards.module').then(m => m.LeaderboardsModule),
     title: 'Finder | LeaderBoards',
   },
   {
     path: 'messages',
-    loadComponent: () => import('./pages/messages/messages.component').then(m => m.MessageComponent),
+    loadComponent: () => import('./pages/messages/messages.component').then(m => m.MessagesComponent),
     title: 'Finder | Messages',
     data: {preload: true}
   },
--- src/app/pages/leaderboards/leaderboards.module.ts
+++ src/app/pages/leaderboards/leaderboards.module.ts
@@ -1,15 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {LeaderboardsComponent} from "./leaderboards.component";
-
+import {OnlineStatusDirective} from "../../directives/online-status.directive";
+import {SortByGemCountPipe} from "../../pipes/sort-by-gem-count.pipe";
+import {LeaderboardsRoutingModule} from "./leaderboards-routing.module";
 
 
 @NgModule({
   declarations: [
-    LeaderboardsComponent
+    LeaderboardsComponent,
+
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    LeaderboardsRoutingModule,
+    OnlineStatusDirective,
+    SortByGemCountPipe
   ]
 })
-export class LeaderboardsModule { }
+export class LeaderboardsModule {
+}
