8057e7710ebab764c0db949756369a9c616a37de
Denisa Kita <denisakita7@gmail.com>
2024-03-13 22:34:37 +0100

    add some html

--- src/app/services/preloading.service.ts
+++ src/app/services/preloading.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {PreloadingStrategy, Route} from "@angular/router";
+import {Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class PreloadingService {
+export class PreloadingService implements PreloadingStrategy {
 
-  constructor() { }
+  public preload(route: Route, fn: () => Observable<any>): Observable<any> {
+    return (route.data && route.data['preload']) ? fn() : of(null);
+  }
+
+  constructor() {
+  }
 }
