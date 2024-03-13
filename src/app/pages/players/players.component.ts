82e96bb687e33f9e7156ad86391d7752b95ab47f
Denisa Kita <denisakita7@gmail.com>
2024-03-13 14:03:27 +0100

    add code for api service

--- src/app/pages/players/players.component.ts
+++ src/app/pages/players/players.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable, of} from "rxjs";
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-players',
@@ -10,17 +11,15 @@ export class PlayersComponent implements OnInit {
   public players$: Observable<Player[] | undefined> = of(undefined);
 
   constructor(
-    // private api: ApiService,
+    private api: ApiService,
   ) {
   }
 
   ngOnInit() {
-    // this.players$ = this.api.getAllPlayers$();
-    this.players$ = of(undefined);
+    this.players$ = this.api.getAllPlayers$();
   }
 
   public update(text: string) {
-    // this.players$ = this.api.getPlayersByName$();
-    this.players$ = of(undefined);
+    this.players$ = this.api.getPlayersByName$(text);
   }
 }
--- src/app/pages/players/players.module.ts
+++ src/app/pages/players/players.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PlayersComponent} from "./players.component";
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PlayersComponent],
   imports: [
     CommonModule
   ]
 })
-export class PlayersModule { }
+export class PlayersModule {
+}
